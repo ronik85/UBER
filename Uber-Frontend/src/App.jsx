96b498fa02270cd1da3313b949e6f4d42db62ff3
Ronik <ronik.kalanouria@gmail.com>
2025-01-07 10:21:37 +0530

    Tailwind added

--- Uber-Frontend/src/App.jsx
+++ Uber-Frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     Hello React app
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
